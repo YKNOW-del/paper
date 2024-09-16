07f9b0f7b8d1c0a588e136dc0f7f13ba764c5922
YKNOW-del <2926258650@qq.com>
2024-09-16 23:11:49 +0800

    Add files via upload

---
+++ 0912.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1=1;
+    int sum=0;
+    while(num1<101){
+       sum+=num1;
+        num1++;
+    }
+    cout<<sum<<endl;
+    
+    return 0;
+}
